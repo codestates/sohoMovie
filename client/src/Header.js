50822239811e2b2192a7ebb5c0aad2644af68f00
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-05-10 08:39:47 +0900

    Delete client directory

--- client/src/Header.js
+++
@@ -1,3 +0,0 @@
-export default function Header() {
-  return <h1>Movie Top 10</h1>;
-}
--- client/src/api/movieDataApi.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-// import getQueryString from './getQueryString'; // Advanced Challenge
-
-export function getMovies(params = {}) {
-  // TODO: fetch를 이용해 endpoint로부터 영화 정보를 받아오세요
-  // Advanced Challenge: getQueryString 함수를 이용하여, 전달되는 params에 맞는 적합한 쿼리 파라미터를 포함하여 요청을 보내야 합니다.
-  let endpoint = 'https://okie249pmk.execute-api.ap-northeast-2.amazonaws.com/movies';
-  return;
-}
