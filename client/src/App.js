510e8c1a95a9f6cec7e617eee8efc0755c6cfb82
thecharmingpark <thecharmingprk@gmail.com>
2022-05-14 19:18:32 +0900

    admin done

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./page/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./page/Main";
 import Mypage from "./page/mypage";
-import Admin from "./page/admin";
+import AAdmin from "./page/admin";
 import Login from "./page/login";
 import Logout from "./page/logout";
 import ShoppingCart from "./page/shoppingcart";
@@ -16,14 +16,14 @@ export const App = () => {
     <>
       <Router>
         <NavBar />
-          <Routes>
-            <Route exact path="/" element={<Main/>} />
-            <Route path="/admin" element={<Admin/>} />
-            <Route path="/mypage" element={<Mypage/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/logout" element={<Logout/>} />
-            <Route path="/shoppingcart" element={<ShoppingCart/>} />
-          </Routes>
+        <Routes>
+          <Route exact path="/" element={<Main/>} />
+          <Route path="/admin/*" element={<AAdmin/>} />
+          <Route path="/mypage" element={<Mypage/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/logout" element={<Logout/>} />
+          <Route path="/shoppingcart" element={<ShoppingCart/>} />
+        </Routes>
       </Router>
     </>
   );
