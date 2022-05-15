7471b54c4975e4dc153a7fadd5bedac95d6ffce1
jaymksth <kwj170@gmail.com>
2022-05-15 23:01:26 +0900

    sever_controllers

---
+++ server/routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const controller = require('./controllers');
+
+// userId로 전체 주문 내역을 조회하는 라우팅
+router.get('/:userId/orders', controller.orders.get);
+// 쇼핑 카트에서 새로운 주문을 생성하는 라우팅
+router.post('/:userId/orders/new', controller.orders.post);
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+router.post('/:signup',)
+
+module.exports = router;
