f19eae93acf521053458da57932245e196796728
qkralsrl11 <qkralsrl11@naver.com>
2022-05-17 16:19:25 +0900

    server/mypage

--- server/controllers/users/mypage.js
+++ server/controllers/users/mypage.js
@@ -3,64 +3,90 @@ const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
 
 module.exports = {
 
-myinfo : async (req, res) => {
+  myinfo: async (req, res) => {
     console.log("회원정보가져오기")
-    const userId = req.params.user_id
-  
-    if(!userId){
-        res.status(400).send({ message: '유저아이디값이 잘못되었습니다.' });
-    }else{
-        const { user_id } = req.body;
-        const data = await users.findOne({
-            where: { user_id:user_id, password: password }
-          });
-    }
-res.status(200)
-},
-
-updinfo: async (req, res) => {
-console.log("회원정보수정")
-try {
-    const { user_id, password } = req.body;
-    if (!user_id || !password) {
-      return res.status(422).send("req값 오류");
+    const user_id = req.query.id
+    console.log(user_id)
+    if (!user_id) {
+      res.status(400).send({ message: '유저아이디값이 잘못되었습니다.' });
     } else {
-  
-      await users.update({
-        where: { user_id: user_id },
-        defaults: {
-      
-          password: password,
-   
-        }
-      }).then(([result, created]) => {
-        if (!created) {
-       console.log("created : ", created)
-          return res.status(409).send("회원정보수정오류");
-        } else {
-          console.log("created : ", created)
-
-          const accessToken = generateAccessToken(result.dataValues);
-          console.log(accessToken)
-          console.log("회원정보수정 성공")
-          return res.status(201).cookie("jwt", accessToken).json({ message: '회원정보수정 성공.' })
-          
-         
-        }
-      })
+      const data = await users.findOne({
+        where: { user_id: user_id }
+      }).then(result => {
+        res.status(200).send({ data: result })
+      });
     }
-  } catch (err) {
-    return null;
+
+
+
+  },
+
+  updinfo: async (req, res) => {
+    console.log("회원정보수정")
+    try {
+      const { user_id, password } = req.body;
+      if (!user_id || !password) {
+        return res.status(422).send("req값 오류");
+      } else {
+        console.log("else문입장")
+        await users.update({ password: password }, {
+          where: {
+            user_id: user_id
+          }
+        }).then((result) => {
+          if (!result) {
+            console.log("result", result)
+            return res.status(409).send("회원정보수정오류");
+          } else {
+            console.log("created : ", result)
+            console.log("회원정보수정 성공")
+            return res.status(200).json({ message: '회원정보수정 성공.' })
+          }
+        })
+      }
+    } catch (err) {
+      return null;
+    }
+
+
+  },
+
+  delinfo: async (req, res) => {
+    console.log("회원탈퇴")
+    try {
+      const user_id  = req.query.id
+      console.log(user_id)
+      if (!user_id ) {
+        return res.status(422).send("req값 오류");
+      } else {
+        console.log("else문입장")
+        await users.destroy({
+          where: {
+           user_id :user_id
+          }
+        
+
+        }).then((result) => {
+          if (!result) {
+            console.log("result", result)
+            return res.status(409).send("회원탈퇴오류");
+          } else {
+            console.log("created : ", result)
+
+
+
+            console.log("회원탈퇴 성공")
+            return res.status(200).json({ message: '회원탈퇴성공.' })
+
+
+          }
+        })
+      }
+    } catch (err) {
+      return null;
+    }
+
+
+
   }
-
-
-},
-
-delinfo: async (req, res) => {
-console.log("회원탈퇴")
-
-
-
-
-}
 }
